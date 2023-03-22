273f143126e7f96a743b04b2381894686969e30b
Theo Furtado Mauricio <sistema.lefouetblanc@gmail.com>
2023-03-22 02:57:57 -0300

    Estilo da pagina Editar

--- src/scripts/editar-pedido.js
+++ src/scripts/editar-pedido.js
@@ -8,91 +8,40 @@ function carregarPedido() {
     
     pedidoRef.on("value", (snapshot) => {
         let pedido = snapshot.val();
+        
         for(let i = 0; i < pedido.Pedido.length; i++){
             
-            //console.log(i)
-            if(pedido.Personalizacoes[i] != "" && pedido.Imagens[i] != ""){
-                //Com personalizacao e Com Imagem
-                boxDetalhes.innerHTML += `
-                    <img class="img-pedido" id="img-pedido" src=${pedido.Imagens[i]}>
-                    <input type="text" id="editarPedido${i}" value="${pedido.Pedido[i]}">
-                    <input type="text" id="editarDataEntrega" value="${pedido.DataEntrega}">
+            boxDetalhes.innerHTML += `
+    <img class="img-pedido" id="img-pedido" src=${pedido.Imagens[i]}>
+            <br>
+    <h3>Pedido ${i+1}</h3>
+    <input type="text" id="editarPedido${i}" value="${pedido.Pedido[i]}">
+
+    <h5>Data de entrega</h5>
+    <input type="text" id="editarDataEntrega" value="${pedido.DataEntrega}">
                     <br>
-                    <textarea id="editarDescricao${i}" value='${pedido.Descricao[i]}'></textarea>
+    <h5>Descrição</h5>
+    <textarea id="editarDescricao${i}" value='${pedido.Descricao[i]}'rows="7">
+        ${pedido.Descricao[i]}
+    </textarea>
                     <br>
-                    <input type="text" id="editarValorPedido${i}" value="${pedido.ValorPedido[i]}">
+    <h5>Valor do pedido</h5>
+    <input type="text" id="editarValorPedido${i}" value="${pedido.ValorPedido[i]}">
 
                     <hr class="hr-divisoria"> 
 
-                    <h2>Personalização</h2>
+                    <h3>Personalização ${[i+1]}</h3>
                     <br>
-                    <textarea id="editarPersonalizacao${i}" value="${pedido.Personalizacoes[i]}">
-                    <br>
-                    <input type="text" id="editarValorPedido${i}" value="${pedido.ValorExtra[i]}">
-                    <!--Repetir quantos pedidos forem necessario antes das info do cliente-->
-
-                    <hr class="hr-divisoria">
-                    
-                 `
-            }else if (pedido.Personalizacoes[i] == "" && pedido.Imagens[i] != "") {
-                //Sem personalizacao e Com imagem
-                boxDetalhes.innerHTML += `
-                    <img class="img-pedido" id="img-pedido" src=${pedido.Imagens[i]}>
-                    <input type="text" id="editarPedido${i}" value="${pedido.Pedido[i]}">
-                    <input type="text" id="editarDataEntrega" value="${pedido.DataEntrega}">
-                    <br>
-                    <textarea id="editarDescricao${i}" value='${pedido.Descricao[i]}'></textarea>
-                    <br>
-                    <input type="text" id="editarValorPedido${i}" value="${pedido.ValorPedido[i]}">
-
-
-                    <hr class="hr-divisoria"> 
-                    <!--Repetir quantos pedidos forem necessario antes das info do cliente-->
-                    
-                 `
-            } else if(pedido.Personalizacoes[i] != "" && pedido.Imagens[i] == "" ){
-                //Com personalizacao e sem imagem
-                console.log(pedido.Personalizacoes[i])
-                boxDetalhes.innerHTML += `
+    <textarea id="editarPersonalizacao${i}" value="${pedido.Personalizacoes[i]}" rows="7">
+        ${pedido.Personalizacoes[i]}
+                    </textarea>
+    <input type="text" id="editarValorPedido${i}" value="${pedido.ValorExtra[i]}">
                 
-                    <input type="text" id="editarPedido${i}" value="${pedido.Pedido[i]}">
-                    <input type="text" id="editarDataEntrega" value="${pedido.DataEntrega}">
-                    <br>
-                    <textarea id="editarDescricao${i}" value='${pedido.Descricao[i]}'></textarea>
-                    <br>
-                    <input type="text" id="editarValorPedido${i}" value="${pedido.ValorPedido[i]}">
-
-
                     <hr class="hr-divisoria"> 
-
-                    <h2>Personalização</h2>
-                    <br>
-                    <textarea id="editarPersonalizacao${i}" value="${pedido.Personalizacoes[i]}" placeholder="${pedido.Personalizacoes[i]}"></textarea>
-                    <br>
-                    <input type="text" id="editarValorPedido${i}" value="${pedido.ValorExtra[i]}">
-                    <!--Repetir quantos pedidos forem necessario antes das info do cliente-->
-
-                    <hr class="hr-divisoria">
-                 `
-
-            } else if(pedido.Personalizacoes[i] == "" && pedido.Imagens[i] == ""){
-                //Sem personalizacao e sem imagem
-                boxDetalhes.innerHTML += `
-                
-                    <input type="text" id="editarPedido${i}" value="${pedido.Pedido[i]}">
-                    <input type="text" id="editarDataEntrega" value="${pedido.DataEntrega}">
-                    <br>
-                    <textarea id="editarDescricao${i}" value='${pedido.Descricao[i]}'></textarea>
-                    <br>
-                    <input type="text" id="editarValorPedido${i}" value="${pedido.ValorPedido[i]}">
-
-                    <hr class="hr-divisoria"> 
-                    
-                    <!--Repetir quantos pedidos forem necessario antes das info do cliente-->
-                 `
-            }
-            
+                    <!--FIM PEDIDO-->
+                 `;
         }
+            
         boxDetalhes.innerHTML += `
            
             <h3>Valor Total: </h3>
@@ -101,14 +50,14 @@ function carregarPedido() {
             <h2>Informações do Cliente</h2>
             <br>
             <h5>Nome: </h5>
-            <input type="text" id="EditarNomeCliente" value="${pedido.NomeCliente}">
+            <input type="text" class="inputCliente" id="EditarNomeCliente" value="${pedido.NomeCliente}">
             <br>
             <h5>Contato: </h5>
-            <input type="text" id="EditarContatoCliente" value="${pedido.ContatoCliente}">
+            <input type="text"  class="inputCliente" id="EditarContatoCliente" value="${pedido.ContatoCliente}">
       
             <br>
             <h5>Endereço de Entrega: </h5>
-            <input type="text" id="EditarEnderecoEntrega" value="${pedido.EnderecoEntrega}">
+            <input type="text" id="EditarEnderecoEntrega" class="inputCliente" value="${pedido.EnderecoEntrega}">
             
 
 
@@ -117,11 +66,7 @@ function carregarPedido() {
                 <button class="btGrey2" onclick="btVoltar()">Voltar</button>
             </div> 
         
-        `
-        //console.log(userId)
-        //console.log(pedidoId)
-        //console.log(pedido)
-        // atualiza o conteúdo da página com as informações do pedido
+        `;
     })       
 }      
 
